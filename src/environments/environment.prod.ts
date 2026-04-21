cda1dfa41a236495652ee7d0013cce56a2d7b97b
Rich1999 <70981675+drah9991@users.noreply.github.com>
2026-04-21 14:40:00 -0500

    Fix production environment Firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCt0EYLGDcNukvA0f8xyLKQI-xknPIU52I',
+    authDomain: 'crud-angular-62925.firebaseapp.com',
+    projectId: 'crud-angular-62925',
+    storageBucket: 'crud-angular-62925.appspot.com',
+    messagingSenderId: '316025143068',
+    appId: '1:316025143068:web:5a528a6fe23ead94e984d9'
+  }
 };
